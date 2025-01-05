67df09b5599788db467bf43d5f5ebd1c33482a02
Keegan Conway <keegsconway@gmail.com>
2025-01-05 11:15:08 -0500

    add sound and refactor

--- main.lua
+++ main.lua
@@ -1,11 +1,25 @@
+local TILE_WIDTH, TILE_HEIGHT
+
 function love.load()
+	loadAssets()
+	initializeTilemap()
+	initializePlayer()
+end
+
+function loadAssets()
+	song = love.audio.newSource("song.ogg", "stream")
+	song:setLooping(true)
+	song:play()
+
+	sfx = love.audio.newSource("sfx.ogg", "static")
+
 	image = love.graphics.newImage("tileset.png")
 
 	local image_width = image:getWidth()
 	local image_height = image:getHeight()
 
-	width = (image_width / 3) - 2
-	height = (image_height / 2) - 2
+	TILE_WIDTH = (image_width / 3) - 2
+	TILE_HEIGHT = (image_height / 2) - 2
 
 	quads = {}
 
@@ -14,17 +28,19 @@ function love.load()
 			table.insert(
 				quads,
 				love.graphics.newQuad(
-					1 + j * (width + 2),
-					1 + i * (height + 2),
-					width,
-					height,
+					1 + j * (TILE_WIDTH + 2),
+					1 + i * (TILE_HEIGHT + 2),
+					TILE_WIDTH,
+					TILE_HEIGHT,
 					image_width,
 					image_height
 				)
 			)
 		end
 	end
+end
 
+function initializeTilemap()
 	tilemap = {
 		{ 1, 6, 6, 2, 1, 6, 6, 2 },
 		{ 3, 0, 0, 4, 5, 0, 0, 3 },
@@ -35,7 +51,9 @@ function love.load()
 		{ 3, 0, 0, 1, 2, 0, 0, 3 },
 		{ 4, 6, 6, 5, 4, 6, 6, 5 },
 	}
+end
 
+function initializePlayer()
 	player = {
 		image = love.graphics.newImage("player.png"),
 		tile_x = 2,
@@ -44,10 +62,15 @@ function love.load()
 end
 
 function isEmpty(x, y)
-	return tilemap[y][x] == 0
+	return tilemap[y] and tilemap[y][x] == 0
 end
 
 function love.keypressed(key)
+	if key == "space" then
+		sfx:play()
+		return
+	end
+
 	local x = player.tile_x
 	local y = player.tile_y
 
@@ -71,10 +94,10 @@ function love.draw()
 	for i, row in ipairs(tilemap) do
 		for j, tile in ipairs(row) do
 			if tile ~= 0 then
-				love.graphics.draw(image, quads[tile], j * width, i * height)
+				love.graphics.draw(image, quads[tile], j * TILE_WIDTH, i * TILE_HEIGHT)
 			end
 		end
 	end
 
-	love.graphics.draw(player.image, player.tile_x * width, player.tile_y * height)
+	love.graphics.draw(player.image, player.tile_x * TILE_WIDTH, player.tile_y * TILE_HEIGHT)
 end
