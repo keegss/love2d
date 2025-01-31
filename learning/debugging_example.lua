b7ed6e96b6a8416e0a32b3b1eff57d6ec0e76d95
Keegan Conway <keegsconway@gmail.com>
2025-01-30 22:38:51 -0500

    cleaup

---
+++ learning/debugging_example.lua
@@ -0,0 +1,33 @@
+io.stdout:setvbuf("no")
+
+function love.load()
+	circle = { x = 100, y = 100 }
+	bullets = {}
+end
+
+function love.update(dt)
+	for i, v in ipairs(bullets) do
+		v.x = v.x + 400 * dt
+	end
+end
+
+function love.draw()
+	love.graphics.circle("fill", circle.x, circle.y, 50)
+
+	for i, v in ipairs(bullets) do
+		love.graphics.circle("fill", v.x, v.y, 10)
+		print(v.x)
+	end
+end
+
+function love.keypressed(key)
+	print("What is the key?", key)
+	if key == "space" then
+		shoot()
+	end
+end
+
+function shoot()
+	table.insert(bullets, { x = circle.x, y = circle.y })
+	print("How many bullets?", #bullets)
+end
